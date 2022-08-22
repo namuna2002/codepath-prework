3ea4a27969be176103bed94573cf64de001d2b06
namuna2002 <namunakhadka19@gmail.com>
2022-08-21 22:29:34 -0700

    added pre work to repository

---
+++ PreworkNamuna/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PreworkNamuna
+//
+//  Created by Namuna Khadka on 8/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var TextLabel: UILabel!
+    @IBAction func ButtonClicked(_ sender: Any) {
+        print("hello")
+        TextLabel.textColor = UIColor.orange
+    }
+    
+}
+
